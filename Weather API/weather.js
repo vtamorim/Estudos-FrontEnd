89d3200661f2eeacbee2fe480f25f3bf72938e30
Victor Miguel <vthero89@gmail.com>
2025-08-24 21:25:12 +0000

    Aplicação de Clima

--- Weather API/weather.js
+++ Weather API/weather.js
@@ -1,10 +1,26 @@
-Send_button = document.getElementById("send_place");
-Place = document.getElementById("place");
+const Send_button = document.getElementById("send_place");
+const Place = document.getElementById("place");
+const dados = document.getElementById("dados");
+const apikey = "67f2150649fc4ef8873145241252308";
 
-apikey = "67f2150649fc4ef8873145241252308";
-const response = await fetch('http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${Place}');
-const users = await response.json();
+Send_button.addEventListener("click", async () => {
 
-users.forEach( function(post)  {
+    const response = await fetch(`httpS://api.weatherapi.com/v1/current.json?key=${apikey}&q=${Place.value}`);
+    const users = await response.json();
 
+
+    dados.innerHTML = "";
+
+
+    const cidade = document.createElement("p");
+    cidade.textContent = `Cidade: ${users.location.name}`;
+
+    const temperatura = document.createElement("p");
+    temperatura.textContent = `Temperatura: ${users.current.temp_c} °C`;
+
+    const condicao = document.createElement("p");
+    condicao.textContent = `Condição: ${users.current.condition.text}`;
+
+
+    dados.append(cidade, temperatura, condicao);
 });
