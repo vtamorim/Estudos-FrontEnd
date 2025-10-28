377692c62544e50618a6b415e96f9af8a44818b4
Victor Miguel <vthero89@gmail.com>
2025-10-28 16:34:09 -0300

    Testando o vue-project
    fiz uns papo ae

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Mano o que é isso KKKKKKKKK</h1>
+    <p>Eae</p>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  background-color: #111;
+  color: #42b983;
+  text-align: center;
+  margin-top: 20vh;
+  font-family: sans-serif;
+}
+</style>
